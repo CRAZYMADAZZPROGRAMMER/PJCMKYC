a97acce0400f2f10db5b89b681192cfb46e90b81
Phillip Martin <chefpj4x@gmail.com>
2026-03-13 07:34:08 -0700

    Fixed DefaultConnection String And Added New Area

---
+++ Areas/Campaigning/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PJCMKYC.Areas.Campaigning.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
